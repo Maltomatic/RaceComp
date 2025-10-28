cc3cb2cd626def1c4c627e1b8fc03cb8b449b6f0
Maltomatic <jjbugman2468@gmail.com>
2025-10-28 15:34:55 -0700

    pixel shuffle to replace ConvTranspose for models; default on

--- models/cnn_abridged_unet.py
+++ models/cnn_abridged_unet.py
@@ -10,17 +10,17 @@ class conv_block(nn.Module):
         super().__init__()
         self.block = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, kernel_size=ker, padding=pad, stride=1),
-            nn.BatchNorm2d(out_ch,),
+            # nn.BatchNorm2d(out_ch,),
             nn.SiLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, kernel_size=ker, padding=pad, stride=1),
-            nn.BatchNorm2d(out_ch),
+            # nn.BatchNorm2d(out_ch),
             nn.SiLU(inplace=True)
         )
     def forward(self, x):
         return self.block(x)
 
 class Resnet_upscaler(nn.Module):
-    def __init__(self, output_size=(224,224)):
+    def __init__(self, output_size=(224,224), px_shuffle = True):
         "Model structure: take 3*112*112 tensor, upscale to 3*224*224 tensor"
         super().__init__()
         self.resnet = torchvision.models.resnet.resnet50(weights=torchvision.models.ResNet50_Weights.IMAGENET1K_V2)
@@ -44,11 +44,27 @@ class Resnet_upscaler(nn.Module):
         self.dec2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
         self.conv_up2 = conv_block(256, 256)
         
-        self.dec1 = nn.ConvTranspose2d(256, 64, kernel_size=2, stride=2)
-        self.conv_up1 = conv_block(64, 64)
+        self.dec1 = None
+        self.conv_up1 = None
+        self.exit = None
+        self.final = None
+        if(not px_shuffle):
+            self.dec1 = nn.ConvTranspose2d(256, 64, kernel_size=2, stride=2)
+            self.conv_up1 = conv_block(64, 64)
+            self.exit = nn.Conv2d(64, 3, kernel_size=3, stride=1)
+            self.resize = nn.Upsample(size=output_size, mode="bilinear", align_corners=False)
+        else:
+            self.dec1 = nn.Sequential(
+                nn.Conv2d(256, 64*(2**2), kernel_size=4, padding=4, dilation = 4, stride=1),
+                nn.PixelShuffle(2),
+                nn.Conv2d(64, 64*(2**2), kernel_size=3, padding=1, stride=1),
+                nn.PixelShuffle(2)
+            )
+            self.conv_up1 = conv_block(64, 3*(2**2))
+            self.exit = nn.PixelShuffle(2)
+            self.resize = nn.Conv2d(3, 3, kernel_size=3, padding = 1, stride=1)
 
-        self.exit = nn.Conv2d(64, 3, kernel_size=3, stride=1)
-        self.resize = nn.Upsample(size=output_size, mode="bilinear", align_corners=False)
+        
     
     def forward(self, x):
         #encoder
@@ -75,22 +91,22 @@ class Resnet_upscaler(nn.Module):
         # print("    d4 after conv shape:", d4.shape)
 
         d3 = self.dec3(d4)
-        # print("d3 shape:", d3.shape)
-        # print("x3 shape:", x3.shape)
+        print("d3 shape:", d3.shape)
+        print("x3 shape:", x3.shape)
         d3c = torch.cat([d3, x3], dim=1)
-        # print("    d3 concatted shape:", d3c.shape)
+        print("    d3 concatted shape:", d3c.shape)
         d3 = self.conv_up3(d3c)
-        # print("    d3 after conv shape:", d3.shape)
+        print("    d3 after conv shape:", d3.shape)
 
         d2 = self.dec2(d3)
-        # print("d2 shape:", d2.shape)
+        print("d2 shape:", d2.shape)
         d2 = self.conv_up2(d2)
-        # print("    d2 after conv shape:", d2.shape)
+        print("    d2 after conv shape:", d2.shape)
         
         d1 = self.dec1(d2)
-        # print("d1 shape:", d1.shape)
+        print("d1 shape:", d1.shape)
         d1 = self.conv_up1(d1)
-        # print("    d1 after conv shape:", d1.shape)
+        print("    d1 after conv shape:", d1.shape)
         
         out = self.exit(d1)
         out = self.resize(out)
@@ -102,7 +118,7 @@ if __name__ == "__main__":
     print("Availability: ", device)
     if(torch.cuda.is_available()):
         print(f"GPU ID: {torch.cuda.current_device()}, {torch.cuda.get_device_name(torch.cuda.current_device())}")
-    model = Resnet_upscaler().to(device)
+    model = Resnet_upscaler(px_shuffle=True).to(device)
     summary(model, (3, 112, 112))
     x = torch.randn(1, 3, 112, 112)
     y = model(x.to(device))
